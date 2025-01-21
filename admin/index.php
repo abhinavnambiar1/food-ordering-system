4479825964f8f4a89e58f8415868ce5a2f739928
Abhinav Nambiar <abhinavdinesankp@gmail.com>
2025-01-22 00:34:17 +0530

    basic database structure and started building frontend

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Ordering System</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- Menu Section Starts -->
+        <div class="menu">
+            <div class="wrapper">
+                Menu is here
+            </div>
+        </div>
+    <!-- Menu Section Ends -->
+
+    <!-- Main Content Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                Main Section
+            </div>
+        </div>
+    <!-- Main Content Ends -->
+
+    <!-- Footer Section Starts -->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><span id="current-year"></span> | All rights reserved | Abhinav Nambiar</p>
+            </div>
+        </div>
+    <!-- Footer Section Ends -->
+     <script src="../js/admin.js"></script>
+</body>
+</html>
---
+++ js/admin.js
@@ -0,0 +1,7 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Insert the year into the span element
+document.getElementById("current-year").textContent = currentYear;
+
+
